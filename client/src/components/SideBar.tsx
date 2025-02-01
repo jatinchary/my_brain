1b194a6b1d69f215cb9bbce2e0ffcab4610c6ad6
jatinchary <charyjatin@gmail.com>
2025-02-01 17:52:22 +0530

    changes done

--- client/src/components/SideBar.tsx
+++ client/src/components/SideBar.tsx
@@ -1,4 +1,4 @@
-import { Brain } from "../icons/Icons";
+import { Brain, Document, Linkicon2, Twitter, YouTube } from "../icons/Icons";
 
 
 export default function Sidebar() {
@@ -18,9 +18,7 @@ export default function Sidebar() {
           className="p-3 rounded-lg hover:bg-[#0045e6] focus:outline-none focus:ring-2 focus:ring-blue-500"
           aria-label="Twitter"
         >
-          <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z" />
-          </svg>
+        <Twitter/>
         </button>
 
         {/* YouTube button */}
@@ -28,9 +26,7 @@ export default function Sidebar() {
           className="p-3 rounded-lg hover:bg-[#0045e6] focus:outline-none focus:ring-2 focus:ring-red-500"
           aria-label="YouTube"
         >
-          <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" />
-          </svg>
+          <YouTube/>
         </button>
 
         {/* Article button */}
@@ -38,20 +34,7 @@ export default function Sidebar() {
           className="p-3 rounded-lg hover:bg-[#0045e6] focus:outline-none focus:ring-2 focus:ring-green-500"
           aria-label="Articles"
         >
-          <svg
-            className="w-8 h-8"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"
-            />
-          </svg>
+         <Document/>
         </button>
 
         {/* Link button */}
@@ -59,20 +42,7 @@ export default function Sidebar() {
           className="p-3 rounded-lg hover:bg-[#0045e6] focus:outline-none focus:ring-2 focus:ring-yellow-500"
           aria-label="Links"
         >
-          <svg
-            className="w-8 h-8"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
-            />
-          </svg>
+         <Linkicon2/>
         </button>
       </nav>
     </aside>
